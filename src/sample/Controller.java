8d9251c1c5989b306b79bc7ad9e33476e19bbf93
Nejla Sabotic <nsabotic1@etf.unsa.ba>
2020-10-15 20:36:26 +0200

    initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,4 @@
+package ba.unsa.etf.predavanje02;
+
+public class Controller {
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.predavanje02;
+
+
+
+public class Main{
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println("halo");
+    }
+}
